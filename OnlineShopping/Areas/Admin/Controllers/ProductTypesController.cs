b5b0fea9bcc733231ffca64742fcc40f1f52a3f9
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-05-24 02:42:11 +0300

    edit delete işlemleri yapıldı

--- OnlineShopping/Areas/Admin/Controllers/ProductTypesController.cs
+++ OnlineShopping/Areas/Admin/Controllers/ProductTypesController.cs
@@ -21,19 +21,97 @@ namespace OnlineShopping.Areas.Admin.Controllers
             return View(_db.ProductTypes.ToList());
         }
    
-        //Create Get Action Method
-        public ActionResult Create()
+        //Edit Get Action Method
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            
+            
+            return View(productType);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         //Create Post Action Method
-        public async Task<IActionResult> Create(ProductTypes productTypes)
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
         {
             if (ModelState.IsValid)
             {
-                _db.ProductTypes.Add(productTypes);
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+        //Details Get Action Method
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(productType);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Details Post Action Method
+        public IActionResult Details(ProductTypes productTypes)
+        {
+           
+                return RedirectToAction(nameof(Index));
+         
+        }
+        //Delete Get Action Method
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(productType);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Delete Post Action Method
+        public async Task<IActionResult> Delete(int id, ProductTypes productTypes)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            if (id != productTypes.Id)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Remove(productType);
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
