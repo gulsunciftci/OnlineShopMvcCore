3e23384dc8a6cc2c2c7741815375d2cbc857b5ad
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-05-26 03:11:24 +0300

    product edit sayfası eklendi

--- OnlineShopping/Areas/Admin/Controllers/ProductsController.cs
+++ OnlineShopping/Areas/Admin/Controllers/ProductsController.cs
@@ -16,6 +16,7 @@ namespace OnlineShopping.Areas.Admin.Controllers
     {
         private ApplicationDbContext _db;
         private IHostingEnvironment _he;
+
         public ProductsController(ApplicationDbContext db, IHostingEnvironment he)
         {
             _db = db;
@@ -42,9 +43,56 @@ namespace OnlineShopping.Areas.Admin.Controllers
                 await image.CopyToAsync(new FileStream(name, FileMode.Create));
                 products.Image = "Images/" + image.FileName;
             }
+            if (image == null)
+            {
+                products.Image = "Images/noimage.jpeg";
+            }
             if (ModelState.IsValid)
             {
                 _db.Products.Add(products);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product has been saved";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+        //Edit Get Action Method
+        public ActionResult Edit(int? id)
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "SpecialTag");
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c=>c.ProductTypes).Include(f=>f.SpecialTags).ToList().FirstOrDefault(c=>c.Product==id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Create Post Action Method
+        public async Task<IActionResult> Edit(Products products, IFormFile image)
+        {
+            if (image != null)
+            {
+                var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                products.Image = "Images/" + image.FileName;
+            }
+            if (image == null)
+            {
+                products.Image = "Images/noimage.jpeg";
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(products);
                 await _db.SaveChangesAsync();
                 TempData["save"] = "Product has been saved";
                 return RedirectToAction(nameof(Index));
