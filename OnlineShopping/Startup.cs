548d868892135973a5b072872174cf30734fdb68
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-05-27 21:01:35 +0300

    sepet eklendi müşteriler için vitrin oluşturuldu

--- OnlineShopping/Startup.cs
+++ OnlineShopping/Startup.cs
@@ -28,11 +28,19 @@ namespace OnlineShopping
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                //options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -63,7 +71,7 @@ namespace OnlineShopping
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.UseSession();
             app.UseAuthentication();
 
             app.UseMvc(routes =>
